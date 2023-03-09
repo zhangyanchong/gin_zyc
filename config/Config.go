9cd774df5245749f783c201d807a0e9414313d65
zhangsan1 <510466486@qq.com>
2023-03-09 14:37:20 +0800

    解析json

---
+++ config/Config.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+)
+
+//解析json文件
+func ParseConfig(path string) (map[string]interface{}, error) {
+	AppPath, _ := os.Getwd()
+	content, err := ioutil.ReadFile(AppPath + "/config/" + path + ".json")
+	if err != nil {
+		return nil, err
+	}
+	// Now let's unmarshall the data into `payload`
+	var payload map[string]interface{}
+	err = json.Unmarshal(content, &payload)
+
+	if err != nil {
+		return nil, err
+	}
+	return payload, nil
+}
--- main.go
+++ main.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"fmt"
+	"gin_zyc/config"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 func main() {
+
 	// 1.创建路由
 	r := gin.Default()
+
+	appJson, err := config.ParseConfig("app")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(appJson["app_port"])
 	// 2.绑定路由规则，执行的函数
-	// gin.Context，封装了request和response
-	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "hello World!")
-	})
-	// 3.监听端口，默认在8080
-	// Run("里面不指定端口号默认为8080")
-	r.Run(":8000")
+	//// gin.Context，封装了request和response
+	//r.GET("/", func(c *gin.Context) {
+	//	c.String(http.StatusOK, "hello World!")
+	//})
+
+	r.Run(":" + appJson["app_port"].(string))
 }
