2fc0eda04ab46da33d7aa93e3b8741c123c6a7b5
zhangsan1 <510466486@qq.com>
2023-03-10 14:22:33 +0800

    new

--- app/tools/config.go
+++ app/tools/config.go
@@ -4,15 +4,13 @@ import (
 	"encoding/json"
 	"gin_zyc/config"
 	"io/ioutil"
-	"os"
 )
 
 var _cfg *config.App = nil
 
 //解析json文件
 func ParseConfig(path string) (*config.App, error) {
-	AppPath, _ := os.Getwd()
-	content, err := ioutil.ReadFile(AppPath + "/config/" + path + ".json")
+	content, err := ioutil.ReadFile("./config/" + path + ".json")
 	if err != nil {
 		return nil, err
 	}
